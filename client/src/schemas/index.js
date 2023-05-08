ff9a31ed931756626624cf7b5ca493290f64b3c0
jaytamrakar <jaytamrakar5@gmail.com>
2023-05-09 01:28:53 +0530

    Change the confirm password validation schema

--- client/src/schemas/index.js
+++ client/src/schemas/index.js
@@ -1,8 +1,28 @@
 import * as Yup from "yup";
 
-export const signUpSchema = Yup.object({
-    name : Yup.string().min(3).required("Please enter your name"),
-    email : Yup.string().email().required("Please enter your email"),
-    password : Yup.string().min(6).required("Please enter your password"),
-    // confirm_Password : Yup.string().required().oneOf([Yup.ref("password"), null], "Passwords must match")
-})
+const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/;
+
+export const signUpValidationSchema = Yup.object({
+    name: Yup.string()
+        .min(3, "Please enter you real name")
+        .required("Please enter your name"),
+
+    email: Yup.string()
+        .email("Please enter a valid email address")
+        .required("Please enter your email"),
+
+    password: Yup.string()
+        .matches(PASSWORD_REGEX, "Please enter a strong password")
+        .min(6, "Password must be at least 6 characters")
+        .required("Please enter your password"),
+
+    confirmPassword: Yup.string()
+        .required("Please confirm your password")
+        .when("password", {
+            is: (val) => (val && val.length > 0 ? true : false),
+            then: Yup.string().oneOf(
+                [Yup.ref("password"), null],
+                "Password does not match"
+            ),
+        }),
+});

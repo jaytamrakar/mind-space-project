988d7990e02348a0db35bdefbbc2bd8c39abd256
mratul07 <atuld67861@gmail.com>
2023-05-14 15:00:59 +0530

    Doctor details info card image section update

--- client/src/components/DoctorInfo.jsx
+++ client/src/components/DoctorInfo.jsx
@@ -1,41 +1,56 @@
 import React from "react";
+import { SlGraduation } from "react-icons/sl";
+import { FaBeer } from "react-icons/fa";
+import { SlCamrecorder } from "react-icons/sl";
+import { GrChat } from "react-icons/gr";
+import { BsFillTelephoneFill } from "react-icons/bs";
 
 const DoctorInfo = () => {
   return (
     <>
-      <div className="flex w-screen h-screen border-4 border-red-600 m-2">
+      <div className="flex max-w-fit h-screen  m-2">
         {/* left Div */}
-        <div className="flex flex-col border-4 border-yellow-600 m-2 w-1/4">
-          <div>
-            <div className="photo-wrapper ">
+        <div className="flex flex-col  m-2 w-1/4">
+          <div className=" bg-slate-300 border-4 rounded-xl shadow-xl">
+            <div className="flex justify-center">
               <img
                 className="w-50 h-50 border-4  rounded-full my-4 md:p-4 "
                 src="https://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp"
                 alt="Photo"
               />
             </div>
-            <div className="text-center m-2 p-4 border-4 border-pink-700 m-2">
+            <div className="text-center m-2 p-4">
               <h3 className="text-xl font-medium">Janhavi Melinkeri</h3>
               <p className="p-4">
                 MA in Clinical Psychology & MSc. in Applied Child Psychology
               </p>
             </div>
+            <div className="flex justify-center flex-wrap space-x-8 p-5">
+              <SlCamrecorder/>
+              <GrChat/>
+              <BsFillTelephoneFill/>
+            </div>
           </div>
           <div className="flex flex-col justify-end items-center ">
-            <button className="bg-blue-700 text-white hover:bg-indigo-400 font-bold py-2 px-4 mt-3 rounded">Book Session</button>
+            <button className="bg-blue-700 text-white hover:bg-indigo-400 font-bold py-2 px-4 mt-3 rounded">
+              Book Session
+            </button>
           </div>
         </div>
 
         {/* Right Div */}
-        <div className="flex flex-col  border-4 border-blue-600 m-2 w-3/4">
-          <div className="flex flex-row justify-evenly flex-wrap border-2  border-amber-950 ">
-            <div className="border-4 border-lime-600 m-2 p-4 ">
-              <h1 className="text-xl font-bold">Qualification</h1>
-              <p className="my-2 py-4">
-                MA In Clinical Psychology & MSc. In Applied Child Psychology
-              </p>
+        <div className="flex flex-col   m-2 w-3/4">
+          <div className="flex flex-row justify-evenly flex-wrap ">
+            <div>
+              <SlGraduation />
+              <div className=" ">
+                <h1 className="text-xl font-bold">Qualification</h1>
+                <p className="my-2 py-4">
+                  MA In Clinical Psychology & MSc. In Applied Child Psychology
+                </p>
+              </div>
             </div>
-            <div className="border-4 border-cyan-500 m-2 p-4 max-w-md">
+            <div className=" m-2 p-4 max-w-md">
               <h1 className="text-xl font-bold">Specialization</h1>
               <p className="my-2 py-4">
                 MA In Clinical Psychology & MSc. In Applied Child PsychAnxiety,
@@ -44,17 +59,20 @@ const DoctorInfo = () => {
                 Depression, Grief & Lossology
               </p>
             </div>
-            <div className="border-4 border-cyan-500 m-2 p-4 min-w-max max-w-md">
+            <div className=" m-2 p-4 min-w-max max-w-md">
               <h1 className="text-xl font-bold">Languages Spoken</h1>
               <p className="my-2 py-4">English, Marathi, Hindi</p>
             </div>
 
-            <div className="border-4 border-cyan-500 m-2 p-4 min-w-md">
-              <h1 className="text-xl font-bold">Next Available at</h1>
-              <p className="my-2 py-4">May 7, 2023 4:00 PM</p>
+            <div className="flex py-0">
+              <FaBeer />
+              <div className="  min-w-md">
+                <h1 className="text-xl font-bold">Next Available at</h1>
+                <p className="my-2 py-4">May 7, 2023 4:00 PM</p>
+              </div>
             </div>
 
-            <div className="border-4 border-cyan-500 m-2 p-4 max-w-md">
+            <div className=" m-2 p-4 max-w-md">
               <h1 className="text-xl font-bold">Expertise</h1>
               <p className="my-2 py-4">
                 Client Centred, Mindfulness Based Therapies, Play And Art
@@ -64,7 +82,7 @@ const DoctorInfo = () => {
             </div>
           </div>
 
-          <div className="border-4 border-black  overflow-hidden">
+          <div className="overflow-hidden">
             <div className="m-2">
               <h1 className="text-xl font-bold">About</h1>
               <p className="flex flex-col justify-end items-center h-full py-2">

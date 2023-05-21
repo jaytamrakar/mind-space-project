238956eada6195457d8b20fc5061f70e6a746254
mratul07 <atuld67861@gmail.com>
2023-05-21 21:43:55 +0530

    Dashboard
    Dashboard with react icons

--- client/src/components/webRtc/main.js
+++ client/src/components/webRtc/main.js
@@ -1,192 +1,192 @@
-let APP_ID = "6cb90178d7a444bbb2173f6b8d01a425"
+// let APP_ID = "6cb90178d7a444bbb2173f6b8d01a425"
 
 
-let token = null;
-// this is for each user to get a unique id 
-let uid = String(Math.floor(Math.random() * 10000))
+// let token = null;
+// // this is for each user to get a unique id 
+// let uid = String(Math.floor(Math.random() * 10000))
 
-let client;
-// this is our room
-let channel;
+// let client;
+// // this is our room
+// let channel;
 
-let queryString = window.location.search
-let urlParams = new URLSearchParams(queryString)
-let roomId = urlParams.get('room')
+// let queryString = window.location.search
+// let urlParams = new URLSearchParams(queryString)
+// let roomId = urlParams.get('room')
 
- if(!roomId){
-//     window.location = 'lobby.html'
- }
+//  if(!roomId){
+// //     window.location = 'lobby.html'
+//  }
 
-let localStream;
-let remoteStream;
-let peerConnection;
+// let localStream;
+// let remoteStream;
+// let peerConnection;
 
-// stun servers from google
-const servers = {
-    iceServers:[
-        {
-            urls:['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
-        }
-    ]
-}
+// // stun servers from google
+// const servers = {
+//     iceServers:[
+//         {
+//             urls:['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302']
+//         }
+//     ]
+// }
 
-let constraints = {
-    video:{
-        width:{min:640, ideal:1920, max:1920},
-        height:{min:480, ideal:1080, max:1080},
-    },
-    audio:true
-}
+// let constraints = {
+//     video:{
+//         width:{min:640, ideal:1920, max:1920},
+//         height:{min:480, ideal:1080, max:1080},
+//     },
+//     audio:true
+// }
 
-// this will start everthing up in our page
-let init = async () =>{
+// // this will start everthing up in our page
+// let init = async () =>{
 
-    client = await AgoraRTM.createInstance(APP_ID)
-    await client.login({uid, token})
+//     client = await AgoraRTM.createInstance(APP_ID)
+//     await client.login({uid, token})
 
-    // url?room=12342
-    channel = client.createChannel(roomId)
-    await channel.join()
+//     // url?room=12342
+//     channel = client.createChannel(roomId)
+//     await channel.join()
 
-    channel.on('MemberJoined', handleUserJoined)
-    channel.on('MemberLeft', handleUserLeft)
+//     channel.on('MemberJoined', handleUserJoined)
+//     channel.on('MemberLeft', handleUserLeft)
 
-    client.on('MessageFromPeer', handleMessageFromPeer)
+//     client.on('MessageFromPeer', handleMessageFromPeer)
 
-    localStream = await navigator.mediaDevices(getUserMedia(constraints)) //video tag in html
-    document.getElementById('user-1').srcObject = localStream;
-}
+//     localStream = await navigator.mediaDevices(getUserMedia(constraints)) //video tag in html
+//     document.getElementById('user-1').srcObject = localStream;
+// }
 
-// for the second user joined
-let handleUserJoined = async (MemberId) => {
-    console.log('A new user joined the channel:', MemberId)
-    createOffer(MemberId)
-}
+// // for the second user joined
+// let handleUserJoined = async (MemberId) => {
+//     console.log('A new user joined the channel:', MemberId)
+//     createOffer(MemberId)
+// }
 
-// for memeber leaving the room
-let handleUserLeft = (MemberId) =>{
-    document.getElementById('user-2').style.display = 'none'
-    document.getElementById('user-1').classList.add('smallFrame')
-}
-
-
-let handleMessageFromPeer = async (message, MemberId) => {
-
-    message = JSON.parse(message.text)
-
-    if(message.type === 'offer'){
-        createAnswer(MemberId, message.offer)
-    }
-
-    if(message.type === 'answer'){
-        addAnswer(message.answer)
-    }
-
-    if(message.type === 'candidate'){
-        if(peerConnection){
-            peerConnection.addIceCandidate(message.candidate)
-        }
-    }
+// // for memeber leaving the room
+// let handleUserLeft = (MemberId) =>{
+//     document.getElementById('user-2').style.display = 'none'
+//     document.getElementById('user-1').classList.add('smallFrame')
+// }
 
 
-}
+// let handleMessageFromPeer = async (message, MemberId) => {
 
-// wrapped the common code here for both offer and answer
-let createPeerConnection = async (MemberId) => {
-    peerConnection = new RTCPeerConnection(servers)
+//     message = JSON.parse(message.text)
 
-    remoteStream = new MediaStream()
-    document.getElementById('user-2').srcObject = remoteStream;
-    document.getElementById('user-2').style.display = 'block';
+//     if(message.type === 'offer'){
+//         createAnswer(MemberId, message.offer)
+//     }
 
-    document.getElementById('user-1').classList.add('smallFrame')
+//     if(message.type === 'answer'){
+//         addAnswer(message.answer)
+//     }
 
-    if(!localStream){
-        localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
-        document.getElementById('user-1').srcObject = localStream
-    }
+//     if(message.type === 'candidate'){
+//         if(peerConnection){
+//             peerConnection.addIceCandidate(message.candidate)
+//         }
+//     }
 
-    // this will keep on listening while we connectd
-    localStream.getTracks().forEach((track) => {
-        peerConnection.addTrack(track, localStream)
-    })
 
-    peerConnection.ontrack = (event) => {
-        event.streams[0].getTracks().forEach((track) => {
-            remoteStream.addTrack(track)
-        })
-    }
+// }
 
-    // for creating ice candidates, as we hit create offer
-    peerConnection.onicecandidate = async (event) => {
-        if(event.candidate){
-            client.sendMessageToPeer({text:JSON.stringify({'type':'candidate', 'candidate':event.candidate})}, MemberId)
-        }
-    }
-}
+// // wrapped the common code here for both offer and answer
+// let createPeerConnection = async (MemberId) => {
+//     peerConnection = new RTCPeerConnection(servers)
 
-// this will create a offer 
-let createOffer = async (MemberId) => {
+//     remoteStream = new MediaStream()
+//     document.getElementById('user-2').srcObject = remoteStream;
+//     document.getElementById('user-2').style.display = 'block';
 
-    await createPeerConnection(MemberId);
+//     document.getElementById('user-1').classList.add('smallFrame')
 
-    let offer = await peerConnection.createOffer()
-    await peerConnection.setLocalDescription(offer)
+//     if(!localStream){
+//         localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:false})
+//         document.getElementById('user-1').srcObject = localStream
+//     }
 
-    // send message to user-2
-    client.sendMessageToPeer({text:JSON.stringify({'type':'offer', 'offer':offer})}, MemberId)
-}
+//     // this will keep on listening while we connectd
+//     localStream.getTracks().forEach((track) => {
+//         peerConnection.addTrack(track, localStream)
+//     })
 
-let createAnswer = async (MemberId, offer) => {
-    await createPeerConnection(MemberId);
+//     peerConnection.ontrack = (event) => {
+//         event.streams[0].getTracks().forEach((track) => {
+//             remoteStream.addTrack(track)
+//         })
+//     }
 
-    await peerConnection.setRemoteDescription(offer)
+//     // for creating ice candidates, as we hit create offer
+//     peerConnection.onicecandidate = async (event) => {
+//         if(event.candidate){
+//             client.sendMessageToPeer({text:JSON.stringify({'type':'candidate', 'candidate':event.candidate})}, MemberId)
+//         }
+//     }
+// }
 
-    let answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer);
+// // this will create a offer 
+// let createOffer = async (MemberId) => {
 
-    client.sendMessageToPeer({text:JSON.stringify({'type':'answer', 'answer':answer})}, MemberId)
-}
+//     await createPeerConnection(MemberId);
 
-let addAnswer = async (answer) => {
-    if(!peerConnection.currentRemoteDescription){
-        peerConnection.setRemoteDescription(answer)
-    }
-}
+//     let offer = await peerConnection.createOffer()
+//     await peerConnection.setLocalDescription(offer)
 
-let leaveChannel = async () => {
-    await channel.leave()
-    await client.logout()
-}
+//     // send message to user-2
+//     client.sendMessageToPeer({text:JSON.stringify({'type':'offer', 'offer':offer})}, MemberId)
+// }
 
-let toggleCamera = async () => {
-    let videoTrack = localStream.getTracks().find(track => track.kind === 'video')
+// let createAnswer = async (MemberId, offer) => {
+//     await createPeerConnection(MemberId);
 
-    if(videoTrack.enabled){
-        videoTrack.enabled = false
-        document.getElementById('camera-btn').style.backgroundColor = 'rgb(255, 80, 80)'
-    }else{
-        videoTrack.enabled = true
-        document.getElementById('camera-btn').style.backgroundColor = 'rgb(179, 102, 249, .9)'
-    }
-}
+//     await peerConnection.setRemoteDescription(offer)
 
-let toggleMic = async () => {
-    let audioTrack = localStream.getTracks().find(track => track.kind === 'audio')
+//     let answer = await peerConnection.createAnswer();
+//     await peerConnection.setLocalDescription(answer);
 
-    if(audioTrack.enabled){
-        audioTrack.enabled = false
-        document.getElementById('mic-btn').style.backgroundColor = 'rgb(255, 80, 80)'
-    }else{
-        audioTrack.enabled = true
-        document.getElementById('mic-btn').style.backgroundColor = 'rgb(179, 102, 249, .9)'
-    }
-}
+//     client.sendMessageToPeer({text:JSON.stringify({'type':'answer', 'answer':answer})}, MemberId)
+// }
 
-// for handling shutdown way of leaving
-window.addEventListener('beforeunload', leaveChannel)
+// let addAnswer = async (answer) => {
+//     if(!peerConnection.currentRemoteDescription){
+//         peerConnection.setRemoteDescription(answer)
+//     }
+// }
 
-document.getElementById('camera-btn').addEventListener('click', toggleCamera)
-document.getElementById('mic-btn').addEventListener('click', toggleMic)
+// let leaveChannel = async () => {
+//     await channel.leave()
+//     await client.logout()
+// }
 
-init();
+// let toggleCamera = async () => {
+//     let videoTrack = localStream.getTracks().find(track => track.kind === 'video')
+
+//     if(videoTrack.enabled){
+//         videoTrack.enabled = false
+//         document.getElementById('camera-btn').style.backgroundColor = 'rgb(255, 80, 80)'
+//     }else{
+//         videoTrack.enabled = true
+//         document.getElementById('camera-btn').style.backgroundColor = 'rgb(179, 102, 249, .9)'
+//     }
+// }
+
+// let toggleMic = async () => {
+//     let audioTrack = localStream.getTracks().find(track => track.kind === 'audio')
+
+//     if(audioTrack.enabled){
+//         audioTrack.enabled = false
+//         document.getElementById('mic-btn').style.backgroundColor = 'rgb(255, 80, 80)'
+//     }else{
+//         audioTrack.enabled = true
+//         document.getElementById('mic-btn').style.backgroundColor = 'rgb(179, 102, 249, .9)'
+//     }
+// }
+
+// // for handling shutdown way of leaving
+// window.addEventListener('beforeunload', leaveChannel)
+
+// document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+// document.getElementById('mic-btn').addEventListener('click', toggleMic)
+
+// init();

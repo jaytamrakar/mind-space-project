0ee2dff4cd8380611be87c4de218ca307a3f792f
jaytamrakar <jaytamrakar5@gmail.com>
2023-05-23 02:44:18 +0530

    different login sidebar changes in dashboard

--- client/src/pages/AboutUs/AboutUs.jsx
+++ client/src/pages/AboutUs/AboutUs.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import DoctorInfo from '../../components/DoctorInfo'
-
 import CheckAvalibility from '../BookingPage/CheckAvalibility'
 import FakeAvl from '../BookingPage/FakeAvl'
 import TimeSlotForm from '../BookingPage/TimeSlotForm'
-
 import ApplyForDoctor from '../ApplyForDoctor/ApplyForDoctor'
-import UserDashboard from '../../components/Dashboard/UserDashboard'
 import OTPVerification from '../OTPVerification'
 import Main from '../../components/Webrtc/Main.jsx'
 import Lobby from '../../components/Webrtc/Lobby'
@@ -21,7 +18,6 @@ const AboutUs = () => {
       <CheckAvalibility />
       <FakeAvl />
       <TimeSlotForm />
-      <UserDashboard />
       <ApplyForDoctor />
       <OTPVerification />
       <ApplyForDoctor />
